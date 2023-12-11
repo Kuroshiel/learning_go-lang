3e654f8dde0f377055aede6a77e46aab655b4b31
Kuroshiel <alifianfikry@gmail.com>
2023-12-11 17:10:19 +0700

    add file named_return_values

---
+++ named_return_values.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// func getCompleteName() (firstName string, middleName string, lastName string)
+func getCompleteName() (firstName, middleName, lastName string) {
+	firstName = "Eko"
+	middleName = "Kurniawan"
+	lastName = "Khannedy"
+
+	return firstName, middleName, lastName
+}
+
+func main() {
+	firstName, middleName, lastName := getCompleteName()
+
+	fmt.Println(firstName, middleName, lastName)
+}
