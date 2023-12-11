ccc0134e358b109751cba9867ba72f5bcaa25630
Kuroshiel <alifianfikry@gmail.com>
2023-12-11 19:10:24 +0700

    Modified File

--- anonymous_function.go
+++ anonymous_function.go
@@ -6,7 +6,7 @@ type Blacklist func(string) bool
 
 func registerUser(name string, blacklist Blacklist) {
 	if blacklist(name) {
-		fmt.Println("You are Blocked", name)
+		fmt.Println("You Are Blocked", name)
 	} else {
 		fmt.Println("Welcome", name)
 	}
