304545d6a95b90f3f6b017d10cf98a739e18f768
Kuroshiel <alifianfikry@gmail.com>
2023-12-14 15:23:46 +0700

    Modified file

--- pointer.go
+++ pointer.go
@@ -7,12 +7,18 @@ type Address struct {
 }
 
 func main() {
-	address1 := Address{"Subang", "Jawa Barat", "Indonesia"}
-	// address2 := address1 // copy value
-	address2 := &address1
+	/* address1 := Address{"Subang", "Jawa Barat", "Indonesia"}
+	address2 := address1 // copy value
 
 	address2.City = "Bandung"
 	fmt.Println(address1) // tidak berubah
+	fmt.Println(address2) // berubah menjadi bandung */
+
+	address1 := Address{"Subang", "Jawa Barat", "Indonesia"}
+	address2 := &address1 // pointer
+
+	address2.City = "Bandung"
+	fmt.Println(address1) // ikut berubah
 	fmt.Println(address2) // berubah menjadi bandung
 
 }
